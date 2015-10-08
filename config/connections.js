0026405cc75fe1cd58d1f9c7da0119731dbbc0f8
Ainur <o.mercher24@gmail.Com>
2015-10-08 17:17:37 +0200

    zzz

--- config/connections.js
+++ config/connections.js
@@ -19,11 +19,11 @@ module.exports.connections = {
     adapter: 'sails-postgresql',
     database: "dm6dfh3k1i39p",
     host: "ec2-54-197-241-239.compute-1.amazonaws.com",
-    user: "dev",
-    password: "dev",
+    user: "fhwdaqjpfygmna",
+    password: "XifQQk2M0X7B39kT_EFVNUjPm8",
     port: 5432,
     pool: false,
-    ssl: false
+    ssl: true,
   },
 
   /**
