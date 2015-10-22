c1bba620732555bdd4207ea885c27c7e6d7b4459
Ainur <o.mercher24@gmail.Com>
2015-10-22 18:01:01 +0200

    yolo on casse tout

--- api/responses/ok.js
+++ api/responses/ok.js
@@ -10,9 +10,7 @@
 module.exports = function (data, code, message, root) {
   // TODO: make transform camelCase to snake_case
   var response = _.assign({
-    code: code || 'OK',
-    message: message || 'Operation is successfully executed',
-    data: data || {}
+       data: data
   }, root);
 
   this.res.status(200);
