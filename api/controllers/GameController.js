c6a31789d60b8dec707013256e402eeffc3e4d5c
Ainur <o.mercher24@gmail.Com>
2015-10-25 18:17:49 +0100

    zz

--- api/controllers/GameController.js
+++ api/controllers/GameController.js
@@ -8,9 +8,8 @@
 module.exports = {
     MyGames: function(req, res) {
 	Game.find()
-	.where({_player1: {userid:req.id}})
+	.where({_player1: {userid:req.param('id')}})
 	.exec(function (err, response) {
-		console.log(err)
 	    });
     
     }
