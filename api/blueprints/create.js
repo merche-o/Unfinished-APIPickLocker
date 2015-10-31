e7d0596bf859cb9f3ce7e5ac32a598ec889049a9
Ainur <o.mercher24@gmail.Com>
2015-10-31 17:23:08 +0100

    o

--- api/blueprints/create.js
+++ api/blueprints/create.js
@@ -7,7 +7,8 @@ var actionUtil = require('sails/lib/hooks/blueprints/actionUtil');
  * An API call to create and return a single model instance using the specified parameters.
  */
 module.exports = function (req, res) {
-  _.set(req.options, 'criteria.blacklist', ['populate']);
+  _.set(req.options, 'criteria.blacklist', ['limit', 'skip', 'sort', 'populate', 'fields']);
+  var fields = req.param('fields') ? req.param('fields').replace(/ /g, '').split(',') : [];
   var populate = req.param('populate') ? req.param('populate').replace(/ /g, '').split(',') : [];
   var values = actionUtil.parseValues(req);
 
