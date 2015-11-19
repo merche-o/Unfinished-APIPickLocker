a417cd9707e5870a4fcd52a462b815480b4413f6
Ainur <o.mercher24@gmail.Com>
2015-11-19 12:52:18 +0100

    update.js

--- api/blueprints/update.js
+++ api/blueprints/update.js
@@ -12,41 +12,11 @@ module.exports = function (req, res) {
   var PK = actionUtil.requirePk(req);
   var values = actionUtil.parseValues(req);
 
-    // Create new instance of model using data from params
-  Model.update(PK, _.omit(values,'id')).exec(function updated (err, newInstance) {
-
-	    // Differentiate between waterline-originated validation errors
-	    // and serious underlying issues. Respond with badRequest if a
-	    // validation error is encountered, w/ validation info.
-	    if (err) return res.negotiate(err);
-
-	    // If we have the pubsub hook, use the model class's publish method
-	    // to notify all subscribers about the created item
-	    if (req._sails.hooks.pubsub) {
-		if (req.isSocket) {
-		    Model.subscribe(req, newInstance);
-		    Model.introduce(newInstance);
-		}
-		Model.publishUpdate(newInstance, !req.options.mirror && req);
-	    }
-
-	    // Send JSONP-friendly response if it's supported
-	    // populate it first
-    Model
-	.findOne({id:newInstance.id})
-	.populateAll()
-	.exec(function(err, populatedInstance){
-		if (err) return res.negotiate(err);
-
-		res.updated(populatedInstance);
-	    });
-	});
-  /*
   Model
     .update(PK, _.omit(values, 'id'))
     .populateAll()
     .then(function (records) {
       return records[0] ? res.ok(records[0]) : res.serverError();
     })
-    .catch(res.serverError);*/
+    .catch(res.serverError);
 };
