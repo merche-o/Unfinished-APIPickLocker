b4eeb33ec386c01b51f7cadbe9996da8e0784f00
Ainur <o.mercher24@gmail.Com>
2015-11-19 13:23:36 +0100

    GameController.js

--- api/controllers/GameController.js
+++ api/controllers/GameController.js
@@ -13,7 +13,7 @@ module.exports = {
 { userid2: req.param('id_user')}
   ]
 	    })
-	    .populate('_player1','_player2')
+	    .populate('_player1')
 	    .populate('_player2')
 	    .exec(function (err, response) {
 		console.log(req.param('id'))
@@ -25,7 +25,8 @@ GamesAvailable: function(req, res) {
     Game.find({userid1: {not : req.param('id_user')}},
 	      {userid2: -1}
 	    )
-	    .populate('_player1','_player2')
+    .populate('_player1')
+    .populate('_player2')
   	.exec(function (err, response) {
 		console.log(req.param('id'))
 		return res.ok(response)
